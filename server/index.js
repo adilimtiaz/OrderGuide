246c80998991d84953d9ccb420dac6e2f5bc95b7
adilimtiaz <adilimtiaz96@gmail.com>
2019-06-26 05:49:04 -0700

    Preparing for production

--- server/index.js
+++ server/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const path = require('path');
 
 require('./models/products');
 const Products = mongoose.model('products');
 
-// Load env variables such as MONGO URI
-require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'production'){
+    // Load env variables such as MONGO URI from .env file
+    // in top level directory
+    require('dotenv').config();
+}
 
 // Mongoose should use native JS promises
 mongoose.Promise = global.Promise;
@@ -19,5 +24,16 @@ app.get('/api/products', async (req, res) => {
     res.send(allProducts);
 });
 
-const PORT = 5000;
+if (process.env.NODE_ENV === 'production'){
+    // If no routes found in express endpoints, check main.js (react build)
+    // for routes that serve files or components
+    app.use(express.static(path.join(__dirname, '../client/build')));
+
+    // If no other route matches, serve index.html in ../client/build
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../client/build/index.html'));
+    });
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
